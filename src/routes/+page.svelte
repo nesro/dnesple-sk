76b98c042e3fc40fa95f7ea4996e2e9e5d6d8d60
nesro <nesro@nesro.cz>
2023-02-22 09:42:04 +0100

    ne

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <h1>Dnes plesk?</h1>
 <p>
-	ANO!
+	ne..
 	<!-- mrkni na kalendar: <a
 		href="https://calendar.google.com/calendar/u/0?cid=ZjUwZGE1MTI5YTk0MjhjOTExYmIwNGQwMDFkYTI2MjQ1Mzc3MWZiYzM5ZmE5YzY0YWVmMWUwNjI2M2EwMGZkMUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t"
 		>https://calendar.google.com/calendar/u/0?cid=ZjUwZGE1MTI5YTk0MjhjOTExYmIwNGQwMDFkYTI2MjQ1Mzc3MWZiYzM5ZmE5YzY0YWVmMWUwNjI2M2EwMGZkMUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t</a
