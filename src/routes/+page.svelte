6457244e0ae0a6867b2e3e1dc7c26cd3e7a69010
nesro <nesro@nesro.cz>
2023-02-27 08:06:41 +0100

    w

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 <p>nevim, mrkni</p>
 <p>
 	kalendar: <a
+		style="color:white"
 		href="https://calendar.google.com/calendar/u/0?cid=ZjUwZGE1MTI5YTk0MjhjOTExYmIwNGQwMDFkYTI2MjQ1Mzc3MWZiYzM5ZmE5YzY0YWVmMWUwNjI2M2EwMGZkMUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t"
 		>https://calendar.google.com/calendar/u/0?cid=ZjUwZGE1MTI5YTk0MjhjOTExYmIwNGQwMDFkYTI2MjQ1Mzc3MWZiYzM5ZmE5YzY0YWVmMWUwNjI2M2EwMGZkMUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t</a
 	>
