10829fd97c68c331012369cb877bade20218052d
nesro <nesro@nesro.cz>
2023-03-05 16:44:03 +0100

    add info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,79 @@
-<h1>Dnes plesk?</h1>
-<p>nevim, mrkni</p>
+<h1>cEDH games in Czechia</h1>
 <p>
-	kalendar: <a
-		style="color:white"
+	cEDH, aka competitive Commander, is a very fun and interactive Magic the Gathering format that is
+	played a lot in Czechia.
+</p>
+<p>
+	I struggled to find people to play with, but to my surprise, there is a lot of small communities
+	that play a lot! The goal of this website is to provide a summary of tournaments, friendly pub
+	games or online games of cEDH. If you want to add your events to the calendar or have a link to
+	your group here, please contact me.
+</p>
+
+<hr />
+
+<ul>
+	<li>
+		<b>(Prague) CZ EDH - Magic The Gathering league</b> every month in Cerny Rytir. Facebook:
+		<a href="https://www.facebook.com/groups/czedh/">https://www.facebook.com/groups/czedh/</a>
+	</li>
+	<li>
+		<b>(Prague) Mana Potion</b> monthly tournaments. Web:
+		<a href="https://www.manapotion.cz/cehd-liga">https://www.manapotion.cz/cehd-liga</a>
+	</li>
+	<li>
+		<b>(Prague) "Soubor" Messenger group</b> chat for arranging cEDH games in Prague. Contact me for
+		invitation. There is over 75 members.
+	</li>
+	<li>
+		<b>(Prague) WhatsApp groups</b> some players are organizing games for Tuesdays, Wednesdays and Thursdays.
+		Contact me for invitation. There is over 90 members.
+	</li>
+	<li>
+		<b>(online) MTG Zasivarna</b> You can find players here for cEDH games over SpellTable with voice
+		chat in czech language. Contact me for invitation to the Discord server. There is over 130 members.
+	</li>
+	<li>
+		<b>(Pilsen) Dragon World</b> web:
+		<a href="https://dragon-world.cz/cedh-liga/">https://dragon-world.cz/cedh-liga/</a>
+	</li>
+	<li>
+		<b>(Hradec Kralove) UNTAP league</b> Facebook page:
+		<a href="https://www.facebook.com/UNTAP.cz">https://www.facebook.com/UNTAP.cz</a>
+	</li>
+	<li>
+		<b>(Uvaly)</b> You can find some cEDH players in Uvaly. Reach them in this Facebook page:
+		<a href="https://www.facebook.com/groups/168893800592267"
+			>https://www.facebook.com/groups/168893800592267</a
+		>
+	</li>
+</ul>
+
+<hr />
+<p>
+	Link to the calendar is here: <a
 		href="https://calendar.google.com/calendar/u/0?cid=ZjUwZGE1MTI5YTk0MjhjOTExYmIwNGQwMDFkYTI2MjQ1Mzc3MWZiYzM5ZmE5YzY0YWVmMWUwNjI2M2EwMGZkMUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t"
-		>https://calendar.google.com/calendar/u/0?cid=ZjUwZGE1MTI5YTk0MjhjOTExYmIwNGQwMDFkYTI2MjQ1Mzc3MWZiYzM5ZmE5YzY0YWVmMWUwNjI2M2EwMGZkMUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t</a
+		>CLICK ME</a
+	>. You can find embedded version of the calendar below.
+</p>
+<p>
+	<iframe
+		title="Google cEDH calendar"
+		src="https://calendar.google.com/calendar/embed?src=f50da5129a9428c911bb04d001da262453771fbc39fa9c64aef1e06263a00fd1%40group.calendar.google.com&ctz=Europe%2FPrague"
+		style="border: 0"
+		width="100%"
+		height="600"
+		frameborder="0"
+		scrolling="no"
+	/>
+</p>
+<hr />
+<p>
+	If you want to contact me, pm me on Facebook: <a href="https://www.facebook.com/nesro"
+		>https://www.facebook.com/nesro</a
 	>
+</p>
+<p>
+	What is "dnes plesk" you ask? "Plesk" is a slang Czech word for "play a game of Magic" and "dnes"
+	means today. So it means "where I can play Magic today". Yes, it's not very funny.
 </p>
